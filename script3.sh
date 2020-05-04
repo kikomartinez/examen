5b64b0496aa57e52bd6d108b5964e63fef89654e
kiko <elok11@gmail.com>
2020-05-04 19:00:58 +0200

    acabado el ejercicio 3

--- script3.sh
+++ script3.sh
@@ -1,20 +1,9 @@
 read -p "numero " num
 
-fin="false"
-
-while [ $fin = "false" ]; do
-    for i in 4 3 2 1 0; do
-	num=$(($num-1))
-    done
-
-    if [ $num -le 0 ]; then
-	fin="true"
-    fi
+while [ $num -ge 5 ]; do
+    num=$((num-5))
 done
 
-
-echo "num vale $num"
-
 if [ $num -eq 0 ]; then
-    echo "es divisible por 5"
+    echo "divisible por 5"
 fi
